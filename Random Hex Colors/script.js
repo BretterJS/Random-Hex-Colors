b03b57726127ac473be10f019d0cf09d124baad4
Brett <18648804+BretterJS@users.noreply.github.com>
2018-07-27 14:12:11 -0500

    Add files via upload

---
+++ Random Hex Colors/script.js
@@ -0,0 +1,29 @@
+const body = document.querySelector('body');
+const container = document.querySelector('#container');
+const hexColorBtn = document.querySelector('#hexColorBtn');
+const hexCode = document.querySelector('#hexCode');
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+hexColorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+
+    let hexColor = '#';
+
+    for (let i = 0; i < 6; i++) {
+        let randomNum = Math.floor(Math.random()*hexNumbers.length);
+        hexColor += hexNumbers[randomNum];
+        // console.log(randomNum);
+        // console.log(hexColor);
+        body.classList.add = 'transitionColor';
+        body.style.backgroundColor = hexColor;
+    
+    }
+
+    setTimeout(() => {
+
+        hexCode.innerHTML = hexColor;
+
+    }, 300);
+   
+};
